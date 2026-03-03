c3eb90332cc25d189cbc8b1c07714af0e44b5514
FedFox <foxyfedor3000@gmail.com>
2026-03-03 17:26:21 +0300

    checkstyle fix 1.0

--- src/main/java/ru/yandex/practicum/filmorate/exeptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeptions/ValidationException.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.exeptions;
 
 import org.slf4j.Logger;
 
-public class ValidationException extends RuntimeException{
+public class ValidationException extends RuntimeException {
 
     public ValidationException(Logger log, final String message) {
         super(message);
